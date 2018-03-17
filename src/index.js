a3b6dec2f2e1e64b042a25f90bf4ba24037b841f
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-03-18 00:09:20 +0100

    Nested expressions - partially working

--- src/index.js
+++ src/index.js
@@ -1,21 +1,65 @@
 import jsx from '@babel/plugin-syntax-jsx'
 
+const flatMap = (iteratee, arr) => [].concat(...arr.map(iteratee))
+const last = arr => (arr.length > 0 ? arr[arr.length - 1] : undefined)
+
 export default ({ types: t }) => {
-  // const buildAdopted = template.expression(`
-  //   <TAG>{CONSUMED => {
-  //     REST
-  //   }}</TAG>
-  // `, {
-  //   plugins: ['jsx'],
-  // })
-  const buildAdopted = ({ TAG, CONSUMED, REST }) => {
+  const tryUnnesting = ({ parentPath, node, key }, state = { success: true, nodes: [node], index: 0, id: null }) => {
+    if (parentPath.isExpressionStatement()) {
+      return state
+    } else if (parentPath.isVariableDeclarator()) {
+      if (state.nodes.length - 1 === state.index) {
+        state.id = parentPath.get('id').node
+      }
+      state.nodes = [...state.nodes.slice(0, -1), t.assignmentExpression(operator, left, last(state.nodes))]
+
+      // const { key } = parentPath
+      // const declaration = parentPath.parentPath
+      // const { kind } = declaration.node
+      // nodes.push(
+      //   declaration
+      //     .get('declarations')
+      //     .map(p => t.variableDeclaration(kind, p.node))
+      // )
+      // return nodes
+      return { success: false }
+    } else if (parentPath.isVariableDeclaration()) {
+      return { success: false }
+    } else if (parentPath.isAssignmentExpression()) {
+      const { operator, left } = parentPath.node
+      if (state.nodes.length - 1 === state.index) {
+        state.id = parentPath.get('left').node
+      }
+      state.nodes = [...state.nodes.slice(0, -1), t.assignmentExpression(operator, left, last(state.nodes))]
+      return tryUnnesting(parentPath, state)
+    } else if (parentPath.isSequenceExpression()) {
+      state.index += key
+      state.nodes = flatMap((p, i) => (i === key ? state.nodes : p.node), parentPath.get('expressions'))
+      return tryUnnesting(parentPath, state)
+    } else {
+      return { success: false }
+    }
+  }
+
+  const buildAdopted = ({ TAG, ADOPTED, REST }) => {
+    /*
+      <TAG>{ADOPTED => {
+        REST
+      }}</TAG>
+    */
     const openingTag = t.cloneNode(TAG)
     openingTag.selfClosing = false
     const closingTag = t.cloneNode(TAG)
     closingTag.type = 'JSXClosingElement'
     return t.jsxElement(openingTag, closingTag, [
-      t.jsxExpressionContainer(t.arrowFunctionExpression([CONSUMED], t.blockStatement(REST))),
+      t.jsxExpressionContainer(t.arrowFunctionExpression([ADOPTED], t.blockStatement(REST))),
     ])
+  }
+
+  const getId = stmt => {
+    if (stmt.isExpressionStatement()) {
+      return stmt.get('expression.left.name').node
+    }
   }
 
   return {
@@ -32,23 +76,34 @@ export default ({ types: t }) => {
           return
         }
 
-        const stmt = path.findParent(p => p.isStatement())
+        const { success, nodes, index, id } = tryUnnesting(path)
 
-        if (path.parentPath.isVariableDeclarator()) {
-          const nextSiblings = stmt.getAllNextSiblings()
-
-          stmt.replaceWith(
-            t.returnStatement(
-              buildAdopted({
-                TAG: consumer.get('openingElement').node,
-                CONSUMED: path.parentPath.get('id').node,
-                REST: nextSiblings.map(p => p.node),
-              }),
-            ),
-          )
-
-          nextSiblings.forEach(path => path.remove())
+        if (success === false) {
+          // generator logic
+          return
         }
+
+        const stmt = path.findParent(p => p.isStatement())
+        const stmtKey = stmt.key
+
+        if (nodes.length > 1) {
+          stmt.replaceWithMultiple(nodes.map(node => t.expressionStatement(node)))
+        }
+
+        const updatedStmt = stmt.getSibling(stmtKey + index)
+        const nextSiblings = updatedStmt.getAllNextSiblings()
+
+        updatedStmt.replaceWith(
+          t.returnStatement(
+            buildAdopted({
+              TAG: consumer.get('openingElement').node,
+              ADOPTED: id,
+              REST: nextSiblings.map(p => p.node),
+            }),
+          ),
+        )
+
+        nextSiblings.forEach(path => path.remove())
       },
     },
   }
